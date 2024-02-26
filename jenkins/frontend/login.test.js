1cd5dbc8a2fadd1276a079b703de52757c0a1a9e
Hyeon-Kyun <kimhyunkyun123@gmail.com>
2024-02-26 16:55:32 +0900

    [Docs] 테스트 코드

--- jenkins/frontend/login.test.js
+++ jenkins/frontend/login.test.js
@@ -2,9 +2,7 @@ import { describe, test, expect, beforeAll, afterAll } from "@jest/globals";
 const { Builder, By } = require("selenium-webdriver");
 const chrome = require("selenium-webdriver/chrome");
 
-describe("회원가입", () => {
-  //   const a = 1,
-  //     b = 2;
+describe("로그인", () => {
   let driver;
 
   beforeAll(async () => {
@@ -13,7 +11,7 @@ describe("회원가입", () => {
       .setChromeOptions(new chrome.Options().addArguments("--headless"))
       .build();
 
-    await driver.get("http://www.bookspedia.kro.kr/");
+    await driver.get("http://192.168.0.141/");
   }, 30000);
 
   afterAll(async () => {
@@ -34,47 +32,5 @@ describe("회원가입", () => {
 
     const mypageBtn = await driver.findElements(By.className("mypageBtn"));
     expect(mypageBtn).not.toBe(null);
-
-    // expect(a + b).toEqual(3);
   });
-  //   test("ID 입력 안했을 때 테스트", () => {
-  //     const { Builder, By } = require("selenium-webdriver");
-
-  //     (async function example() {
-  //       let driver = await new Builder().forBrowser("chrome").build();
-  //       await driver.get("http://www.bookspedia.kro.kr/");
-
-  //       const login_btn1 = await driver.findElement(By.id("loginBtn"));
-  //       login_btn1.click();
-
-  //       const input_pw = await driver.findElement(By.id("passwordText"));
-  //       input_pw.sendKeys("qwer1234!!");
-
-  //       const login_btn2 = await driver.findElements(By.className("login-box-form-button"));
-  //       login_btn2[0].click();
-  //     })();
-
-  //     // expect(a + b + 2).toEqual(3);
-  //   });
-  //   test("PW 틀렸을 때 테스트", () => {
-  //     const { Builder, By } = require("selenium-webdriver");
-
-  //     (async function example() {
-  //       let driver = await new Builder().forBrowser("chrome").build();
-  //       await driver.get("http://www.bookspedia.kro.kr/");
-
-  //       const login_btn1 = await driver.findElement(By.id("loginBtn"));
-  //       login_btn1.click();
-
-  //       const input_id = await driver.findElement(By.id("emailText"));
-  //       input_id.sendKeys("kimhyunkyun123@gmail.com");
-  //       const input_pw = await driver.findElement(By.id("passwordText"));
-  //       input_pw.sendKeys("qwer1234!!!!");
-
-  //       const login_btn2 = await driver.findElements(By.className("login-box-form-button"));
-  //       login_btn2[0].click();
-  //     })();
-
-  //     // expect(a + b).toEqual(3);
-  //   });
 });
